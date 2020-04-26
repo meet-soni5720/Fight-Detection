1fe045800012be137208aaeb8bb47811eefa1b28
parth222 <parth222@users.noreply.github.com>
2020-04-26 21:48:31 +0530

    Update way2sms.py

--- sms/way2sms.py
+++ sms/way2sms.py
@@ -4,7 +4,7 @@ import json
 URL = 'https://www.sms4india.com/api/v1/sendCampaign'
 
 # get request
-def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+def sendPostRequest( apiKey, secretKey, phoneNo, senderId, textMessage,reqUrl= URL,useType='stage'):
   req_params = {
   'apikey':apiKey,
   'secret':secretKey,
@@ -16,11 +16,9 @@ def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textM
   return requests.post(reqUrl, req_params)
 
 # get response
-response = sendPostRequest(URL, 'Y0CRJP21JW1CEOE9MNKJ2PD1U2RQKMDC', 'XD326W5W51BT16D4', 'stage', '9265983776', 'Parth', 'message-text' )
+response = sendPostRequest('Y0CRJP21JW1CEOE9MNKJ2PD1U2RQKMDC', 'XD326W5W51BT16D4', '9265983776', 'Parth', 'message-text' )
 """
   Note:-
     you must provide apikey, secretkey, usetype, mobile, senderid and message values
     and then requst to api
 """
-# print response if you want
-print (response.text)
